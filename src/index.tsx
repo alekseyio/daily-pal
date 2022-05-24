b0da87ec82ad076373899c2fb739360aaa7b95e2
Aleksey Kuznetsov <alekseyk7v@gmail.com>
2022-05-24 22:31:16 +0400

    Eslint setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from '@app/App';
 
-createRoot(document.getElementById('app')!).render(
+const appContainerEl = document.getElementById('app');
+
+if (!appContainerEl) {
+  throw new Error('App container element is missing');
+}
+
+createRoot(appContainerEl).render(
   <StrictMode>
     <App />
   </StrictMode>,
